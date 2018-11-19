a3bc5d4502c2a9f3c2f49ca34619eec2e484a3c9
daneparke <dapa8522@colorado.edu>
2018-11-19 15:04:32 -0700

    Basic enemy drop items

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const app = express()
+const port = process.env.Port || 3001
+
+// const morgan = require('morgan')
+// const bodyParser = require('body-parser')
+const cors = require('cors')
+
+app.use(cors())
+// app.use(morgan('dev'))
+// app.use(bodyParser.json())
+
+const enemies = require('./data.json')
+
+app.get('/enemies', (req, res) => {
+    res.json({ enemies })
+})
+
+// app.use((err, req, res, next) => {
+//     const status = err.status || 500
+//     res.status(status).json({ error: err })
+// })
+
+// app.use((req, res, next) => {
+//     res.status(404).json({ message: 'Not found.' })
+// })
+
+const listener = () => console.log(`Listening on port ${port}`)
+app.listen(port, listener)
