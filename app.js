e7af076a2bf3b90e910ae560945905b9771d168f
daneparke <dapa8522@colorado.edu>
2018-11-22 13:28:22 -0700

    added delete, and fixed the PUT function

--- app.js
+++ app.js
@@ -3,16 +3,45 @@ const app = express()
 const port = process.env.Port || 3001
 
 // const morgan = require('morgan')
-// const bodyParser = require('body-parser')
+const bodyParser = require('body-parser')
 const cors = require('cors')
 
 app.use(cors())
 // app.use(morgan('dev'))
-// app.use(bodyParser.json())
-const player = require('./player.json')
+app.use(bodyParser.json())
+let players = require('./players.json')
 
-app.get('/player', (req, res) => {
-    res.json({ player })
+app.get('/players', (req, res) => {
+    res.json({ players })
+})
+
+app.get('/players/:id', (req, res, next) => {
+    const { id } = req.params
+    const player = players.filter(player => player.id == id)
+    if (player.length < 1) {
+        next()
+    }
+    res.send({ data: player[0] })
+})
+
+app.put('/players', (req, res, next) => {
+    let { id } = req.body
+    let { body } = req
+    let existingName = players.map(player => {
+        if (player.id === id) {
+            player = body
+        }
+        return player
+    })
+    players = existingName
+    console.log({ players })
+    res.send({ players })
+})
+
+app.post('/', (req, res, next) => {
+    let newPlayer = req.body
+    players.push(newPlayer)
+    res.send(newPlayer)
 })
 
 const enemies = require('./enemies.json')
@@ -21,14 +50,23 @@ app.get('/enemies', (req, res) => {
     res.json({ enemies })
 })
 
-// app.use((err, req, res, next) => {
-//     const status = err.status || 500
-//     res.status(status).json({ error: err })
-// })
+app.delete('/players/:id', (req, res) => {
+    let { id } = req.params
+    let newObj = players.filter(player => {
+        return player.id != id
+    })
+    players = newObj
+    res.send({ players })
+})
 
-// app.use((req, res, next) => {
-//     res.status(404).json({ message: 'Not found.' })
-// })
+app.use((req, res, next) => {
+    res.status(404).json({ message: 'Not found.' })
+})
+
+app.use((err, req, res, next) => {
+    const status = err.status || 500
+    res.status(status).json({ error: err })
+})
 
 const listener = () => console.log(`Listening on port ${port}`)
 app.listen(port, listener)
